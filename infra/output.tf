63429ec484865ee1f84843f6bd0aade48fe5cb52
Stoo <stoo.goff@gmail.com>
2025-04-06 17:27:19 +0100

    Added url output

--- infra/output.tf
+++ infra/output.tf
@@ -2,3 +2,7 @@
 output "cdn_url" {
 	value = "https://${bunnynet_pullzone.cdn.name}.${bunnynet_pullzone.cdn.cdn_domain}"
 }
+
+output "url" {
+	value = "https://${bunnynet_dns_record.dns.name}.${data.bunnynet_dns_zone.dns.domain}"
+}
