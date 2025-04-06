f880b7dc88593802c31cf972d3a8cb5a6af850b1
Stoo <stoo.goff@gmail.com>
2025-04-06 16:46:10 +0100

    Failed to save file

--- infra/dns.tf
+++ infra/dns.tf
@@ -1,12 +1,12 @@
 
 data "bunnynet_dns_zone" "dns" {
-	domain = "storytel.la"
+	domain = "wildrpg.com"
 }
 
 resource "bunnynet_dns_record" "dns" {
 	zone = data.bunnynet_dns_zone.dns.id
 
-	name  = ""
+	name  = "www"
 	type  = "CNAME"
 	value = "${bunnynet_pullzone.cdn.name}.${bunnynet_pullzone.cdn.cdn_domain}"
 }
