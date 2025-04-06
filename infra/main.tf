ff38bf15a5b7853b31b24a1ca00c7fe4b88db3ad
Stoo <stoo.goff@gmail.com>
2025-04-06 20:57:07 +0100

    Added error handling rules

--- infra/main.tf
+++ infra/main.tf
@@ -4,23 +4,25 @@ locals {
 }
 
 resource "bunnynet_storage_zone" "www" {
-	name = local.service_name
-	zone_tier = "Standard"
-	region = "UK"
-	replication_regions = ["BR", "NY", "SG"]
+	name                 = local.service_name
+	zone_tier            = "Standard"
+	region               = "UK"
+	replication_regions  = ["BR", "NY", "SG"]
+	custom_404_file_path = "/index.html"
+	rewrite_404_to_200   = true
 }
 
 resource "bunnynet_pullzone" "cdn" {
-	name = local.service_name
+	name         = local.service_name
 	cors_enabled = false
 
 	origin {
-		type = "StorageZone"
+		type        = "StorageZone"
 		storagezone = bunnynet_storage_zone.www.id
 	}
 
 	routing {
-		tier = "Standard"
+		tier  = "Standard"
 		zones = ["AF", "ASIA","EU", "SA", "US"]
 	}
 }
